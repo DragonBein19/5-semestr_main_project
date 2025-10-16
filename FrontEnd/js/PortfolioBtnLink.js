42d08e87826eb7af50ec22c070884ce740f5218c
Doragonbein <salunovas.nikita@gmail.com>
2025-10-16 14:23:21 +0300

    V0,73

--- FrontEnd/js/PortfolioBtnLink.js
+++ FrontEnd/js/PortfolioBtnLink.js
@@ -6,15 +6,22 @@ for(let i =0; i < portfolioList.length; i++)
     const a = portfolioList[i].querySelector("i")
     a.addEventListener("click", function ()
         {
-            if(a.classList.contains("fa-chevron-down"))
+            if(portfolioList[i].querySelector("i").classList.contains("fa-chevron-down"))
             {
-                a.classList.remove("fa-chevron-down")
-                a.classList.add("fa-chevron-up")
+                portfolioList[i].querySelector("i").classList.remove("fa-chevron-down")
+                portfolioList[i].querySelector("i").classList.add("fa-chevron-up")
             }
             else
             {
-                a.classList.remove("fa-chevron-up")
-                a.classList.add("fa-chevron-down")
+                for(let j = 0; j < portfolioList.length; j++)
+                {
+                    if(portfolioList[j].querySelector("i").classList.contains("fa-chevron-up"))
+                    {
+                        portfolioList[j].querySelector("i").classList.remove("fa-chevron-up")
+                        portfolioList[j].querySelector("i").classList.add("fa-chevron-down")
+                    }
+                }
             }
-        })
+        }
+    )
 }
